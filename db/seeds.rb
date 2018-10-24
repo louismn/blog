9510f9a1a51e3c4f19db28f651c79badba9e297d
beeant8 <pdelatour7@gmail.com>
2018-10-24 15:33:22 +0200

    Ajout seeds et faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do 
+  user = User.create!(firstname:Faker::Name.first_name, lastname:Faker::Name.last_name, email:Faker::Internet.email)
+end
+
+10.times do |index|
+	article = Article.create!(title:"Title#{index}", content:"content#{index}")
+end
+
+15.times do |index|
+	comment = Comment.create!(content:"Content#{index}")
+end
+15.times do |index|
+	like = Like.create!(id: "#{index}")
+end
