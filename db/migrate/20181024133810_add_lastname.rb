1f81af1f48eb9d0732de8f198eb1dd86d4713545
beeant8 <pdelatour7@gmail.com>
2018-10-24 15:59:26 +0200

    update seed

---
+++ db/migrate/20181024133810_add_lastname.rb
@@ -0,0 +1,5 @@
+class AddLastname < ActiveRecord::Migration[5.2]
+  def change
+  	add_column :users, :lastname, :string 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_123319) do
+ActiveRecord::Schema.define(version: 2018_10_24_133810) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_10_24_123319) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "lastname"
   end
 
 end
